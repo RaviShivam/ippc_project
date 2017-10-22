698a15d594ea133625288d3e3b040abc33bc6b6e
Ravi Shivam Autar <ravi_autar27@hotmail.com>
2017-10-22 13:35:17 +0200

    Add storage of benchmarks for easy access

--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -1,18 +1,20 @@
 package benchmark;
-import mcts.MCTSSolver;
-import parser.*;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import cache_performance.*;
+import parser.POMDP;
+import parser.ParsePOMDP;
 
 public class Benchmark {
 	
 	private ArrayList<POMDP> testProblems;
-	
+
+	private PrintWriter writer;
+
 	private void loadPOMDP() {
 		this.testProblems = new ArrayList<POMDP>();
 		this.testProblems.add(ParsePOMDP.readPOMDP("domains/hallway.POMDP"));
@@ -26,21 +28,33 @@ public class Benchmark {
 		Metrics metrics = new Metrics(solver);
 		metrics.tic();
 		solver.Solve();
-		metrics.toc();
+		metrics.toc(this.writer);
+	}
+
+	private void initPrintWriter(String fileName) throws IOException {
+		writer = new PrintWriter(new FileWriter(fileName));
+		writer.println("solver; problem; memory (MB); time (s)");
+	}
+
+	private void closePrintWriter() {
+		this.writer.flush();
+		this.writer.close();
 	}
 
 	public ArrayList<POMDP> getTestProblems() {
 		return testProblems;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Benchmark benchmark = new Benchmark();
 		benchmark.loadPOMDP();
+		benchmark.initPrintWriter("results/result.csv");
 		for (POMDP problem : benchmark.getTestProblems()) {
 //			benchmark.benchmark(new MCTSSolver(problem));
 			benchmark.benchmark(new VI(problem));
 			benchmark.benchmark(new VI_FeasibleActions(problem));
 			benchmark.benchmark(new VI_CachePerformance(problem));
 		}
+		benchmark.closePrintWriter();
     }
 }
--- src/benchmark/Metrics.java
+++ src/benchmark/Metrics.java
@@ -2,6 +2,8 @@ package benchmark;
 
 import cache_performance.Solver;
 
+import java.io.PrintWriter;
+
 public class Metrics {
 
     private long initialMemory;
@@ -21,6 +23,7 @@ public class Metrics {
     }
 
     public void tic() {
+        this.runTime.gc();
         System.out.println();
         System.out.printf("===== Benchmarking %s algorithm on the %s problem =====", solver.getSolverName(), solver.getProblemName());
         System.out.println();
@@ -28,15 +31,17 @@ public class Metrics {
         initialMemory = runTime.totalMemory() - runTime.freeMemory();
     }
 
-    public void toc() {
-        long memoryUsage = this.initialMemory - (runTime.totalMemory()-runTime.totalMemory());
-        long timeConsumption = System.currentTimeMillis() - this.initialTime;
-        System.out.printf("Memory consumption: %.3f MB", bytesToMegabytes(memoryUsage));
+    public void toc(PrintWriter writer) {
+        double memoryUsage = bytesToMegabytes(this.initialMemory - (runTime.totalMemory()-runTime.totalMemory()));
+        double timeConsumption = (System.currentTimeMillis() - this.initialTime)/SECOND;
+        System.out.printf("Memory consumption: %.3f MB", memoryUsage);
         System.out.println();
-        System.out.printf("Time consumption: %.3f second", timeConsumption/SECOND);
+        System.out.printf("Time consumption: %.7f second", timeConsumption);
         System.out.println();
         System.out.println("====== Benchmark complete =======");
         System.out.println();
+        writer.printf("%s; %s; %.3f; %.4f", solver.getSolverName(), solver.getProblemName(), memoryUsage, timeConsumption);
+        writer.println();
         runTime.gc();
     }
 }
